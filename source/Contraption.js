14ad17799bf7c6477b4dc236a1f74be337c1914d
Chris Fankhauser <seretogis@gmail.com>
2011-10-04 23:00:08 -0700

    removed splash pane, added container

--- source/Contraption.js
+++ source/Contraption.js
@@ -1,25 +1,24 @@
 enyo.kind({
     name: "UbeCake.Contraption", kind: "Pane", flex: 1, transitionKind: "enyo.transitions.LeftRightFlyin",
     components: [
-        {name: "splash", kind: "VFlexLayout",
+        {name: "contraptionContainer", kind: "Pane",
         components: [
-            {content: "Splash! Whoaa oohhh!"}
-        ]},
-        {name: "initialSetup", kind: "Pane",
-        components: [
-            {kind: "Button", caption: "OK"},
-            {kind: "Button", caption: "Cancel"}            
-        ]},
-        {name: "mainScreen", kind: "Pane",
-        components: [
-            {kind: "Button", caption: "Main"},
-            {kind: "Button", caption: "Screen"}
-        ]},
+            {name: "initialSetup", kind: "Pane",
+            components: [
+                {kind: "Button", caption: "OK"},
+                {kind: "Button", caption: "Cancel"}
+            ]},
+            {name: "mainScreen", kind: "Pane",
+            components: [
+                {kind: "Button", caption: "Main"},
+                {kind: "Button", caption: "Screen"}
+            ]},
+        ]}
     ],
     create: function() {
         this.inherited(arguments);
-        this.$.initialSetup.selectViewByName("splash");  
     },
     ready: function() {
-    }
+        this.$.contraptionContainer.selectViewByName("initialSetup");
+    },
 });
