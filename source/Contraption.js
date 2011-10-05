e7e3ee8833fb8cd9d163d1ec167a773a8a55fedd
Chris Fankhauser <seretogis@gmail.com>
2011-10-04 22:23:27 -0700

    Cleanup in progress..

--- source/Contraption.js
+++ source/Contraption.js
@@ -1,45 +1,25 @@
 enyo.kind({
-  name: "Contraption.Main",
-  kind: enyo.VFlexBox,
-  components: [
-      {kind: "PageHeader", components: [
-          {kind: enyo.VFlexBox, content: "Contraption", flex: 1},
-          {name: "backButton", kind: "Button", content: "Back", onclick: "goBack"}
-      ]},
-      {name: "pane", kind: "Pane", flex: 1, onSelectView: "viewSelected",
-          components: [
-              {name: "search", className: "enyo-bg", kind: "MyApps.Search",
-                  onSelect: "feedSelected", onLinkClick: "linkClicked"},
-              {name: "detail", className: "enyo-bg", kind: "Scroller",
-                  components: [
-                      {name: "webView", kind: "WebView", className: "enyo-view"}
-                  ]
-              }
-          ]
-      }
-  ],
-  create: function() {
-      this.inherited(arguments);
-      this.$.pane.selectViewByName("search");
-  },
-  feedSelected: function(inSender, inFeed) {
-      this.$.pane.selectViewByName("detail");
-      this.$.webView.setUrl(inFeed.link);
-  },
-  linkClicked: function(inSender, inUrl) {
-      this.$.webView.setUrl(inUrl);
-      this.$.pane.selectViewByName("detail");
-  },
-  viewSelected: function(inSender, inView) {
-      if (inView == this.$.search) {
-          this.$.webView.setUrl("");
-          this.$.backButton.hide();
-      }
-      else if (inView == this.$.detail) {
-          this.$.backButton.show();
-      }
-  },
-  goBack: function(inSender, inEvent) {
-      this.$.pane.back(inEvent);
-  }
+    name: "UbeCake.Contraption", kind: "Pane", flex: 1, transitionKind: "enyo.transitions.LeftRightFlyin",
+    components: [
+        {name: "splash", kind: "VFlexLayout",
+        components: [
+            {content: "Splash! Whoaa oohhh!"}
+        ]},
+        {name: "initialSetup", kind: "Pane",
+        components: [
+            {kind: "Button", caption: "OK"},
+            {kind: "Button", caption: "Cancel"}            
+        ]},
+        {name: "mainScreen", kind: "Pane",
+        components: [
+            {kind: "Button", caption: "Main"},
+            {kind: "Button", caption: "Screen"}
+        ]},
+    ],
+    create: function() {
+        this.inherited(arguments);
+        this.$.initialSetup.selectViewByName("splash");  
+    },
+    ready: function() {
+    }
 });
